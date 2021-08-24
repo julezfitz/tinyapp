7e81d864a22b04fddab9b482c7383ffdc0f22c6f
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-08-24 19:42:48 +0000

    fixes issue with shortURL look up and redirect

--- express_server.js
+++ express_server.js
@@ -54,8 +54,15 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
+  const shortURL = generateRandomString(); //Generates new tiny url
+  urlDatabase[shortURL] = req.body.longURL; //Adds it to the database
   console.log(urlDatabase);
   res.redirect(`/urls/${shortURL}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  console.log(req.params.shortURL);
+  const longURL = urlDatabase[req.params.shortURL]; //Looks up long URL from database
+  console.log(longURL);
+  res.redirect(longURL);
 });
