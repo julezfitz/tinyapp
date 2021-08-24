c93120404d2764e85db03b652ea1252138243d10
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-08-24 16:04:47 +0000

    create url_index.ejs and urls page

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -22,4 +23,9 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
