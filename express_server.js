3b1dafdae1e313ba00a4c759bf010b94960670f2
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-08-24 19:20:59 +0000

    generate a random string with a post request, add to url database and redirect to new short url

--- express_server.js
+++ express_server.js
@@ -54,5 +54,8 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const newURL = generateRandomString();
+  urlDatabase[newURL] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect(307, `/urls/:${newURL}`);
 });
